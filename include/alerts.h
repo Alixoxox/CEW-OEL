59b4d80d27b6423d66e5050907ee1ac1cbe1ea19
Alixoxox <Alixoxox@users.noreply.github.com>
2024-11-18 13:48:54 +0500

    u

--- include/alerts.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef ALERTS_H
-#define ALERTS_H
-
-void check_alerts(double temperature, double humidity,double feels_like);
-
-#endif
--- include/file_io.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef FILE_IO_H
-#define FILE_IO_H
-#include "api.h"
-
-void write_raw_data(const char *data);
-void write_processed_data(const struct WeatherData *data);
-
-#endif
